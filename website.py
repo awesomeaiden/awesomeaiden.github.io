37f19bb2a772b359de5e047d573efbe221f4f84a
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2020-06-11 22:55:36 -0400

    After third tutorial -> static site ready:

--- website.py
+++ website.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", content=["tim", "joe", "bill"])
+    return render_template("index.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run()
