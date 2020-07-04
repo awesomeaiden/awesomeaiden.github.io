148a1457c973590a3db0bc200a7705149af27ed5
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2020-07-04 18:12:58 -0400

    More home page shenanigans

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, redirect, url_for, render_template
+import os, json, jsonpickle
+
+app = Flask(__name__)
+
+images_folder = os.path.join('static/images/')
+
+class Project(object):
+    def __init__(self, name="name", desc="desc", image="image", link="link"):
+        self.name = name
+        self.desc = desc
+        self.image = image
+        self.link = link
+
+    def __repr__(self):
+        return "Project: " + self.name
+
+def json2projs(json_file):
+    json_dict = json.load(json_file)
+    projects = []
+    for item in json_dict:
+        projects.append(Project(
+            name=json_dict[item]['name'],
+            desc=json_dict[item]['desc'],
+            image=json_dict[item]['image'],
+            link=json_dict[item]['link']
+        ))
+    return projects
+
+
+@app.route("/")
+@app.route("/index")
+def home():
+    projects = None
+    with open('projects.json') as json_file:
+        projects = json2projs(json_file)
+    print(projects)
+    return render_template("index.html", images=images_folder, projects=projects)
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run()
