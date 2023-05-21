ccca575349fa3db51a1bdfacdb6bcc3fc38e7d26
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2023-05-21 19:03:52 -0400

    Make build.sh work for 'python' or 'python3' command

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 echo Building and pushing website
 rm -r docs/*
 pip install -r requirements.txt
-python app.py build
+python app.py build || python3 app.py build
 cp CNAME docs
 git add docs
 git commit -m "Docs build update"
