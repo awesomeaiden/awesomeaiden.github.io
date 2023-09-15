fb1f220c398a942972e8ec53c1755bf33c360cee
Aiden Gonzalez <aidengonzalez@precocityllc.com>
2023-09-15 01:02:19 -0400

    Update resume, gitignore, and build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 echo Building and pushing website
 rm -r docs/*
-pip install -r requirements.txt
+pip install -r requirements.txt || pip3 install -r requirements.txt
 python app.py build || python3 app.py build
 cp CNAME docs
 git add docs
